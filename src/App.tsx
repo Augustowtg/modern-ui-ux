5db4977afb9a80eb140a6b996d4bc93cf633acf0
Augusto Wesley <augustowtg@gmail.com>
2022-09-03 00:13:29 -0300

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className='bg-primary w-full '>
-        <h1>teste</h1>
+    <div className='bg-primary w-full overflow-hidden '>
+        <h1>Hello, World!</h1>
     </div>
   )
 }
